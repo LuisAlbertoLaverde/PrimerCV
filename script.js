02e18e540524d9fb5a48fd25d76b3ebc68bf38f8
Alberto <unumnio@gmail.com>
2022-11-25 20:37:10 -0600

    Javascript skills section

--- script.js
+++ script.js
@@ -16,7 +16,22 @@ function select(){
     menuVisible=false;
 }
 //Funcion que aplica las animaciones de las habilidades
-
+function sKillsEffect(){
+    var skills=document.getElementById("skills");
+    var skills_distance=window.innerHeight-skills.getBoundingClientRect().top;
+    if (skills_distance>=300) {
+        let skills = document.getElementsByClassName("progress");
+        skills[0].classList.add("cSharp");      
+        skills[1].classList.add("javascript");      
+        skills[2].classList.add("htmlCss");      
+        skills[3].classList.add("unity");      
+        skills[4].classList.add("gitGithub");      
+        skills[5].classList.add("photoshop");      
+        skills[6].classList.add("adobeIllustrator");      
+        skills[7].classList.add("toonBoon");      
+        skills[8].classList.add("english");      
+    }
+}
 
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll=function(){
